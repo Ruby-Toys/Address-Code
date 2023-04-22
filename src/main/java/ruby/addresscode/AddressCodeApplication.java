59bf06aa1343735f03ac2e738a6170a1553caccf
MisterRuby <ruby8700@naver.com>
2023-04-23 03:02:11 +0900

    init : 프로젝트 생성

---
+++ src/main/java/ruby/addresscode/AddressCodeApplication.java
@@ -0,0 +1,13 @@
+package ruby.addresscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ruby/addresscode/AddressCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package ruby.addresscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
